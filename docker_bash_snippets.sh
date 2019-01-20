f2d0be68d3bd44c1457fb82f498eaf906070d377
root <root@ip-172-31-44-37.ap-southeast-1.compute.internal>
2019-01-20 13:40:57 +0000

    new file:   docker_bash_snippets.sh

---
+++ docker_bash_snippets.sh
@@ -0,0 +1,2 @@
+docker build -t centos7-systemd-nginx-sclo-php72 .
+docker run --privileged --name centos7-systemd-nginx-sclo-php72-test -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 80:80 -d centos7-systemd-nginx-sclo-php72
